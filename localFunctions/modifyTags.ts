e424a393b93e837ca17a16528b30339de930eaf5
martijn van de ree <mrtjnvanderee@gmail.com>
2021-11-10 13:29:39 +0100

    tags

--- localFunctions/modifyTags.ts
+++ localFunctions/modifyTags.ts
@@ -1,10 +1,10 @@
 export const modifyTags = (tags: string[]) => {
-  //console.log("tags",tags)
-  return tags.length === 0 ? []: t(tags)
+
+  return tags.length === 0 ? [] : t(tags)
 }
 
 const t = (tags: string[]): string[] => {
-  //console.log("tagstags",tags)
-  return  (tags[0].search(",") !== -1 && tags.length === 1) ? tags[0].split(",") : tags
+
+  return (tags[0].search(",") !== -1 && tags.length === 1) ? tags[0].split(",") : tags
 }
 
